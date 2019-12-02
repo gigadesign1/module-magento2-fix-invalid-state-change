ed9d10161600dd62ca4e823a8435e908d67cb398
Mark van der Werf <info@gigadesign.nl>
2019-12-02 10:02:09 +0100

    Fix invalid import

--- Plugin/UpdateQuoteWhenRestrictedPlugin.php
+++ Plugin/UpdateQuoteWhenRestrictedPlugin.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 
 namespace Gigadesign\FixInvalidStateChange\Plugin;
 
-use Gigadesign\LogOrderFailures\Logger\Logger;
+use Gigadesign\FixInvalidStateChange\Logger\Logger;
 
 use Magento\Authorization\Model\UserContextInterface;
 use Magento\Quote\Api\CartManagementInterface;
